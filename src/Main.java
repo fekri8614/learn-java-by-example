3a4be0efa3834a8a1fe230fbe8817f45a6980760
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-09 10:41:29 -0700

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 
-
 public class Main {
     public static void main(String[] args) {
-        int a = 22;
-        if (a >= 22) System.out.println("Hello, world");
+        // write your code here
     }
 }
