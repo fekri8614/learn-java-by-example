2a119b2c683f980d8931d99d354832afc938468d
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-09 00:47:27 -0700

    DoWhileLoop

--- src/sokan_academy/sea6/DoWhileLoop.java
+++ src/sokan_academy/sea6/DoWhileLoop.java
@@ -2,5 +2,10 @@ package sokan_academy.sea6;
 
 public class DoWhileLoop {
     public static void main(String[] args) {
+        int number = 1;
+        do {
+            System.out.println("My number is: " + number);
+            number++;
+        } while (number <= 10);
     }
 }
