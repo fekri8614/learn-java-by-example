e56fa50f2f084d1677111c52c6a43b9d4c03c6a0
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-16 06:29:09 -0700

    season 7

---
+++ src/sokan_academy/sea7/Array.java
@@ -0,0 +1,17 @@
+package sokan_academy.sea7;
+
+public class Array {
+     public static void main(String[] args) {
+         int[] number = new int[3];
+         number[0] = 91816122;
+         number[1] = 91217945;
+         number[2] = 91144423;
+
+         String[] name = new String[3];
+         name[0] = "Iman";
+         name[1] = "Ali";
+         name[2] = "Fardin";
+
+         System.out.println("My first friend is " + name[0] + " and his phone number is " + number[0]);
+     }
+}
---
+++ src/sokan_academy/sea7/OneDimensionalArray.java
@@ -0,0 +1,12 @@
+package sokan_academy.sea7;
+
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+        String[] items = {"Bread", "Cheese", "Butter", "Tea", "Potato", "Tomato", "Onion", "Coffe", "Yogurt Drink", "Mineral Water"};
+        printMessage(items.length + "");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
